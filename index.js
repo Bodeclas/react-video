c3260175246c3c169102b8f96dd0857a877fdab0
Brayan Jesús Salcedo <brayanjsalcedo@gmail.com>
2018-01-16 21:32:14 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Media from './src/playlist/components/media';
+
+// console.log('Hola mundo!')
+
+const app = document.getElementById('app')
+
+// ReactDOM.render(que voy a renderizar, donde lo haré);
+// const holaMundo = <h1>hola Estudiante!</h1>;
+render(<Media /> , app);
+
