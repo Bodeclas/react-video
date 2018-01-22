04f7d4c8761309c3e970fdb57e28c9f50a50d034
Brayan J Salcedo <brayanjsalcedo@gmail.com>
2018-01-22 13:03:33 -0400

    Add icon play component.

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Media from './media.js';
 import './playlist.css';
+import Play from '../../icons/components/play';
 
 function Playlist(props) {
   const playlist = props.data.categories[0].playlist
   console.log(props.data);
   return (
-    <div className="Playlist" > {
-      playlist.map((item) => {
-        return <Media
-          { ...item } key={item.id}
-        />
-      })
-    } </div>
+    <div className="Playlist">
+      <Play
+        size={30}
+        color="red"
+      />
+      {
+        playlist.map((item) => {
+          return <Media
+            { ...item } key={item.id}
+          />
+        })
+      }
+    </div>
   )
 }
 
