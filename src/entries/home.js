af7456071064da6ea2decb181e07d7ef5c05c3f5
Brayan Jesús Salcedo <brayanjsalcedo@gmail.com>
2018-01-28 00:20:42 -0400

    Resolving normalized data to components

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
-import data from '../api.json';
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducer from '../reducers/data';
 
-import normalizedData from '../schemas/index';
+import data from '../schemas/index';
 
-console.log(normalizedData);
+console.log(data);
 
 const initialState = {
   data: {
     ...data,
+    entities: data.entities,
+    categories: data.result.categories,
   },
   search: [],
 };
