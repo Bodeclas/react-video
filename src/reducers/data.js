2071fd5d74719d7a7e54b1568614073dd45fd82d
Brayan J Salcedo <brayanjsalcedo@gmail.com>
2018-02-01 08:46:36 -0400

    Fix state with immutable

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -4,27 +4,16 @@ import { fromJS } from 'immutable';
 const initialState = fromJS({
     entities: schema.entities,
     categories: schema.result.categories,
-    search: [],
+    search: '',
 });
 
 function data(state = initialState, action) {
   switch (action.type) {
-    case 'SEARCH_VIDEO':
-      let results = [];
-      const list = state.data.categories[2].playlist;
-      if (action.payload.query) {
-        results = list.filter(element => element.author.toLowerCase().includes(action.payload.query.toLowerCase()));
-      }
-      {
-        return {
-          ...state,
-          search: results,
-        };
-      }
-      break;
+    case 'SEARCH_VIDEO': {
+      return state.set('search', action.payload.query);
+    }
     default:
       return state;
-      break;
   }
 }
 
