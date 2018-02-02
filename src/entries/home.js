677f3c8075bd56cbe727b966c7f27a2ba9f24ed2
Brayan J Salcedo <brayanjsalcedo@gmail.com>
2018-02-02 18:29:54 -0400

    Add logger middleware

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,30 +1,24 @@
 import React from 'react';
-import { render } from 'react-dom';
 import Home from '../pages/containers/home';
-
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import reducer from '../reducers/index';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Map as map} from 'immutable';
 
-/* import data from '../schemas/index'; */
-
-/* const initialState = {
-  data: {
-    entities: data.entities,
-    categories: data.result.categories,
-    search: [],
-  },
-  modal: {
-    visibility: false,
-    mediaId: null
-  }
-}; */
+const logger = ({ dispatch, getState }) => next => action => {
+	console.log( 'estado anterior:', getState().toJS() )
+	console.log( 'enviando acción:', action)
+	const rslt = next( action )
+	console.log( 'nuevo estado   :', getState().toJS() )
+	return rslt
+}
 
 const store = createStore(
   reducer,
   map(),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(logger),
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const homeContainer = document.getElementById('home-container');
