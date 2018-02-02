c05526353ee74c66d1d235561d72e310e563cf72
Brayan J Salcedo <brayanjsalcedo@gmail.com>
2018-02-02 07:58:12 -0400

    Add actions creators

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -8,20 +8,14 @@ import HandleError from '../../error/containers/handle-error';
 import VideoPlayer from '../../player/containers/video-player';
 import { connect } from 'react-redux';
 import { List as list } from 'immutable';
+import { openModal, closeModal } from '../../actions/index'
 
 class Home extends Component {
   handleOpenModal = id => {
-    this.props.dispatch({
-      type: 'OPEN_MODAL',
-      payload: {
-        mediaId: id
-      }
-    })
+    this.props.dispatch(openModal(id))
   };
   handleCloseModal = event => {
-    this.props.dispatch({
-      type: 'CLOSE_MODAL'
-    })
+    this.props.dispatch(closeModal())
   };
   render() {
     return (
